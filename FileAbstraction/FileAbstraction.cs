b376b769b9f0cb91990bd60b55f6bc90ba6c65c5
Divan van Zyl <divanvanzyl1234@gmail.com>
2022-07-27 14:30:27 +0200

    Add project files.

---
+++ FileAbstraction/FileAbstraction.cs
@@ -0,0 +1,28 @@
+﻿namespace FileAbstraction
+{
+    public static class ExtentionMethods
+    {
+        /// <summary>
+        /// Year, Month, Day, Time
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ymdt(this DateTime dateTime)
+        {
+            return dateTime.ToString("20220727");
+        }
+        public static bool ToTextFile<T>(this T o)
+        {
+            var dt = new DateTime();
+            File.WriteAllText(@$"D:\Default {dt.ymdt()}.txt", o is null ? "" : o.ToString());
+            return true;
+        }
+
+        public static bool ToTextFile<T>(this T o, string fileName)
+        {
+            File.WriteAllText(@$"D:\{fileName}.txt", o is null ? "" : o.ToString());
+            return true;
+        }
+
+    }
+}
---
+++ FileAbtractionDemo/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Hello, World!");
+var s = "Divan";
+"Divan van Zyl".ToTextFile();
+s.ToTextFile("Hello");
