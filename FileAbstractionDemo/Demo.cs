4999a4195d5d4c74fad786d61f6f07dbf912303c
Divan van Zyl <divanvanzyl1234@gmail.com>
2022-07-31 15:05:24 +0200

    Some demo statements

--- FileAbstractionDemo/Demo.cs
+++ FileAbstractionDemo/Demo.cs
@@ -3,24 +3,20 @@
     public class Demo
     {
         public static void Main()
-        {
-            //"This is some text that I wrote to a file!".ToFile();
-            //1024.ToFile();
-            //new Computer().ToFile();
-            //new object().ToFile();
-
-            //2048.ToFile("myNumber!");
+        {            
             4096.ToFile("I l0ve base 2.numbers");
             FileAbstraction.DisplayFile();
-
 
             8192.ToFile(@"..\Bas3 tw0 numb3r5.txt");
             FileAbstraction.DisplayFile("Bas3 tw0 numb3r5.txt");
 
+            "This was found in my user folder".ToFile(@$"C: \Users\{Environment.UserName}\Downloads\\"+ "myFile.txt");
+            FileAbstraction.DisplayFile("myFile.txt");
+
             "You found me!".ToFile("X:\\I hide here.txt");
             FileAbstraction.DisplayFile("I hide here.txt");
 
-            "You found me too!".ToFile(@"C:\AAAFolder1\And I hide here.txt");
+            "You found me too!".ToFile(@"C:\And I hide here.txt");
             FileAbstraction.DisplayFile("And I hide here.txt");
         }
     }
