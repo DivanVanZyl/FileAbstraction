aa4ba6265cc802b49e03fb6d74b1cb35c9146233
Divan van Zyl <divanvanzyl1234@gmail.com>
2022-08-02 20:44:51 +0200

    Started implementing a console app that utilises the FileAbstraction library

---
+++ ConsoleFileOperations/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FileAbstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Count() == 2)
+            {
+                if (args[0] == "r")
+                {
+                     Console.Out.WriteLine(FileAbstraction.ReadFile(args[1]));
+                }
+                else if (args[0] == "d")
+                {
+                    Console.WriteLine(FileAbstraction.ReadFile(args[1]));
+                }
+                else if(args[0] == "w")
+                {
+                    args[1].ToFile();
+                }
+                else
+                {
+                    Console.Error.WriteLine("Only 'r' for read and 'w' for write, are valid operations.");
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine(@"Syntax: f [r/w] [""file name""]");
+            }
+        }
+    }
+}
