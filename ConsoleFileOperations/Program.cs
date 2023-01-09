9d9006aeb4621250f1a09ae8099ea6e619b6124b
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-01-09 15:56:18 +0200

    Changes to install as a global command

--- ConsoleFileOperations/Program.cs
+++ ConsoleFileOperations/Program.cs
@@ -2,41 +2,68 @@
 {
     class Program
     {
+        private static readonly string _helpText = @"This is a file helper command.
+Usage:
+f [r] [""file name""]
+f [w] [""contents""]
+f [w] [""contents""] [""file name""]
+
+Operation Options:
+-r      read text
+-w      write text
+
+Source Target Options:
+You may either have one parameter, which will be data to write with a default name, or read from a default name.
+Or, you may specify both, with the first parameter being the data, and the second the file name to write to or read from." + Environment.NewLine;
         static void Main(string[] args)
         {
-            if(args.Count() == 2)
+            if (args.Count() == 1)
             {
-                if (args[0] == "r")
+                if (args[0].ToLower() == "h" || args[0].ToLower() == "help")
                 {
-                     Console.Out.WriteLine(FileAbstract.ReadFile(args[1]));
+                    Console.Write(_helpText);
                 }
-                else if(args[0] == "w")
+                else
+                {
+                    Console.WriteLine("'" + args[0] + "'" + " is not a valid command.");
+                    Console.Write(_helpText);
+                }
+            }
+            else if (args.Count() == 2)
+            {
+                if (args[0].ToLower() == "r")
+                {
+                    Console.Out.WriteLine(FileAbstract.ReadFile(args[1]));
+                }
+                else if (args[0].ToLower() == "w")
                 {
                     args[1].ToFile();
                 }
                 else
                 {
-                    Console.Error.WriteLine("Only 'r' for read and 'w' for write, are valid operations.");
+                    Console.Error.WriteLine("Only 'r/rb' for read and 'w/wb' for write, are valid operations.");
+                    Console.Write(_helpText);
                 }
             }
-            else if(args.Count() == 3)
+            else if (args.Count() == 3)
             {
-                if (args[0] == "r")
+                if (args[0].ToLower() == "r")
                 {
-                    Console.Out.WriteLine(FileAbstract.ReadFile(args[1]));
+                    Console.Out.WriteLine(FileAbstract.ReadFile(args[2]));
                 }
-                else if (args[0] == "w")
+                else if (args[0].ToLower() == "w")
                 {
                     args[1].ToFile(args[2]);
                 }
                 else
                 {
                     Console.Error.WriteLine("Only 'r' for read and 'w' for write, are valid operations.");
+                    Console.Write(_helpText);
                 }
             }
             else
             {
-                Console.Error.WriteLine(@"Syntax: f [r] [""contents""]" + Environment.NewLine + @"OR: f [w] [""contents""]" + Environment.NewLine + @"OR: f [w] [""contents""] [""file name""]");
+                Console.Write(_helpText);
             }
         }
     }
